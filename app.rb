aa80d061f0c5e03c888d7f8a6f90fb13bf8e182c
sophiahusak <72futbolplayer@gmail.com>
2018-07-17 19:13:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
+ require_relative 'config/environment'
+
+class App < Sinatra::Base
 get '/hello/:id' do
   @name = params[:id]
   "Hello #{@name}!"
 end
+
+
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+#   get '/' do
+#     "Hello World!"
+#   end
+  
+#   get '/name' do 
+#     "My name is ..."
+#   end
+  
+#   get '/hometown' do 
+#     "My hometown is ..."
+#   end
+  
+#   get '/song' do 
+#     "My favorite song is ..."
+#   end
+  
+# end
+
